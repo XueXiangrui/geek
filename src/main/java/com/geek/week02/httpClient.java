80fc2b9cf2de5643c6f0fd2b185884f1b9a6fa89
xiangrui.xue <xiangrui.xue@tongdun.cn>
2021-10-12 18:27:55 +0800

    更改

---
+++ src/main/java/com/geek/week02/httpClient.java
@@ -0,0 +1,28 @@
+package com.geek.week02;
+
+
+import java.io.IOException;
+
+/**
+ * Created by xiangrui.xue on 2021/10/11.
+ */
+public class httpClient {
+    public static CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    // GET 调用
+    public static String getAsString(String url) throws IOException {
+        HttpGet httpGet = new HttpGet(url);
+        CloseableHttpResponse response1 = null;
+        try {
+            response1 = httpclient.execute(httpGet);
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            return EntityUtils.toString(entity1, "UTF-8");
+        } finally {
+            if (null != response1) {
+                response1.close();
+            }
+        }
+    }
+
+}
