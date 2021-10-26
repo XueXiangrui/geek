a06ea46f741812a5325547f1f26af893ed5b7452
xiangrui.xue <xiangrui.xue@tongdun.cn>
2021-10-26 11:08:58 +0800

    增加Annotation方式

--- src/main/java/com/geek/week05/Bean01.java
+++ src/main/java/com/geek/week05/Bean01.java
@@ -1,10 +1,14 @@
 package com.geek.week05;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by xiangrui.xue on 2021/10/25.
  */
+
+//   Annotation方式
+@Component
 public class Bean01 {
 
 //    XML https://blog.csdn.net/echizao1839/article/details/88063013
